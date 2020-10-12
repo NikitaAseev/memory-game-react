cfbb9fadeeb1b1c01cb734e42149e798d29361f2
Nikita <nikita@Nikitas-MBP.Home>
2020-10-12 14:53:02 +0300

    Updates

---
+++ precache-manifest.281fd0b2736a86b1358d617fafc79761.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82b7f470e7f4a304b1e73f47749bd93",
+    "url": "/memory-game-react/index.html"
+  },
+  {
+    "revision": "b4fa23338fa9740d7d36",
+    "url": "/memory-game-react/static/css/main.b9acd6d9.chunk.css"
+  },
+  {
+    "revision": "7b6e7717718b0c1d41e3",
+    "url": "/memory-game-react/static/js/2.05b8c206.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/memory-game-react/static/js/2.05b8c206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fa23338fa9740d7d36",
+    "url": "/memory-game-react/static/js/main.bb53ca8f.chunk.js"
+  },
+  {
+    "revision": "d8ef393e042a117bcfb8",
+    "url": "/memory-game-react/static/js/runtime-main.9c4f0de0.js"
+  },
+  {
+    "revision": "1948aa56aa951506f7d9614d01748092",
+    "url": "/memory-game-react/static/media/bear.1948aa56.png"
+  },
+  {
+    "revision": "53e1f15528d11784e02cdc7317f1309e",
+    "url": "/memory-game-react/static/media/beaver.53e1f155.png"
+  },
+  {
+    "revision": "5ef3ecae3cf85664e9e1b4d0388cab05",
+    "url": "/memory-game-react/static/media/cat.5ef3ecae.png"
+  },
+  {
+    "revision": "a7c276c86d5d8d274b00d1655bf772c6",
+    "url": "/memory-game-react/static/media/chick.a7c276c8.png"
+  },
+  {
+    "revision": "3d53cc0cdb3a708022acdb68ad05a453",
+    "url": "/memory-game-react/static/media/chicken.3d53cc0c.png"
+  },
+  {
+    "revision": "34c75b4720307f69ff3cf3515386a747",
+    "url": "/memory-game-react/static/media/cow.34c75b47.png"
+  },
+  {
+    "revision": "92719be5f32c7036911d838119b28718",
+    "url": "/memory-game-react/static/media/dog.92719be5.png"
+  },
+  {
+    "revision": "1b3936b64f96bcaedbc7fe88b0bd7c9e",
+    "url": "/memory-game-react/static/media/donkey.1b3936b6.png"
+  },
+  {
+    "revision": "c539fc3d262e4054a5846fd71e0c06f5",
+    "url": "/memory-game-react/static/media/duck.c539fc3d.png"
+  },
+  {
+    "revision": "11075bbfe85fb445cbb6911399275895",
+    "url": "/memory-game-react/static/media/elephant.11075bbf.png"
+  },
+  {
+    "revision": "0f6b2638811f1246a455206f426f5f05",
+    "url": "/memory-game-react/static/media/lion.0f6b2638.png"
+  },
+  {
+    "revision": "e92ffeb9197fdcf7f5fadc03e3e0c664",
+    "url": "/memory-game-react/static/media/monkey.e92ffeb9.png"
+  },
+  {
+    "revision": "73cfdfe5b395b2520d5f2126fc2dd57b",
+    "url": "/memory-game-react/static/media/penguin.73cfdfe5.png"
+  },
+  {
+    "revision": "566e5ee93d9e589e03c03dcef16e39f5",
+    "url": "/memory-game-react/static/media/pig.566e5ee9.png"
+  },
+  {
+    "revision": "6033f74418faa96f300fc108e18ae1ad",
+    "url": "/memory-game-react/static/media/walrus.6033f744.png"
+  },
+  {
+    "revision": "495c1851276c1f5304290902d81a8bfe",
+    "url": "/memory-game-react/static/media/zebra.495c1851.png"
+  }
+]);
